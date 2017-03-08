ce43c294667a4e17918fd4d2d7d122465867af66
Mégane Bati <megane.bati@ensimag.grenoble-inp.fr>
2017-03-08 18:11:04 +0100

    Il manque tracer le papillon sinon ça fonctionne :
    On peut tracer la premiere colonne comme il faut et les lignes
    Le main est sale car n'utilise pas la maille :/

--- src/Main.py
+++ src/Main.py
@@ -9,10 +9,51 @@ from math import *
 from Point import *
 from Papillon import *
 
-origine = Point(0, 0, 0)
-premierPapillon = Papillon(origine, 1, pi/(2.5))
+# TODO :  
+# faire le tracé du papillon 7
+#  utiliser la maille pour remplacer premier et p 
+# éventuellement mettre un booleen pair ou  impair à la place des if
 
-origine.afficherPoint()
+longueur = 5
+angle = pi/2.5
+origine = Point(0, 0, 0)
+premierPapillon = Papillon(origine, 1, 1, angle)
 premierPapillon.tracer()
+p1 = premierPapillon
+max = 0
+p = True
+premier = True
+while (max < longueur):
+        #parité dans la maille
+    if (p):
+        voisin = premierPapillon.ajouterPapillonInitColonne(premierPapillon.nm, premierPapillon.ne, pi/2.5)
+    else :
+        voisin = premierPapillon.ajouterPapillonInitColonne(premierPapillon.no, premierPapillon.nm, pi/2.5)
+    if (premier) :
+        p4 = voisin
+    voisin.tracer()
+    max = max +1 
+    premierPapillon = voisin    
+    p = not p
+    premier = False
+    
+max = 0
+while (max < longueur-1):
+    voisin2 = p1.ajouterPapillonHrzt(p4.se)
+    voisin2.tracer()
+    max2 = 0
+    premier = True
+    while (max2 < longueur):
+        v3 = voisin2.ajouterPapillonVert()
+        v3.tracer()
+        if premier :
+            patate = v3
+        max2 = max2 + 1
+        premier = False
+        voisin2 = v3
+    p1 = voisin2
+    p4 = patate
+    max = max + 1
+    
 plt.axis([0, 5, 0, 5])
 plt.show() 
--- src/Papillon.py
+++ src/Papillon.py
@@ -8,19 +8,83 @@ Created on Wed Mar  1 14:57:07 2017
 from Point import *
 
 class Papillon:
-    """Un papillon regroupe 6 points"""
+    """Un papillon regroupe 6 points nommés par leur 'position géographique' """
         
-    
-    
-    def __init__(self, p1, l, angle):
-        """création du premier papillon"""   
+    def __init__(self):
+        self.so = Point()
+        self.no = Point()
+        self.nm = Point()
+        self.ne = Point()
+        self.sm = Point()
+        self.se = Point()
+
+        
+    def __init__(self, p1, lh, lv, angle):
+        """création du premier papillon
+            lv : longueur du coté vertical d'un papillon
+            lh : idem en horizontal 
+            angle : permet de créer un papillon parfait, pi/3 <= angla <= pi/2"""   
+        #assert(pi/3 <= angle)
+        #assert(angle <= pi/2)
+        #assert(lh <= lv/2 /cos(angle))
         self.so = p1        
-        self.no = Point(self.so.x, self.so.y + l, 0) 
-        self.nm = Point(sin(angle)*l + self.no.x, -cos(angle)*l + self.no.y, 0)
-        self.ne = Point(sin(angle)*l + self.nm.x, self.no.y, 0)
-        self.sm = Point(self.so.x + sin(angle)*l, cos(angle)*l + self.so.y, 0)
-        self.se = Point(self.so.x + 2*sin(angle)*l, self.so.y, 0)
+        self.no = Point(self.so.x, self.so.y + lv, 0) 
+        self.nm = Point(sin(angle)*lh + self.no.x, -cos(angle)*lv + self.no.y, 0)
+        self.ne = Point(sin(angle)*lh + self.nm.x, self.no.y, 0)
+        self.sm = Point(self.so.x + sin(angle)*lh, cos(angle)*lv + self.so.y, 0)
+        self.se = Point(self.so.x + 2*sin(angle)*lh, self.so.y, 0)  
+
   
+    def ajouterPapillonInitColonne(self, p1, p2, angle):
+        """Création du papillon voisin de celui en paramètre
+            les longueurs du papillon sont celles de [p1, p2]"""    
+        longueur = p1.distance(p2)
+            
+        if self.nm == p1 and self.ne == p2:
+            so = p1
+            voisin = Papillon(so, longueur, longueur, angle)
+            voisin.so.libre = False
+            voisin.sm.libre = False
+            
+        elif self.no == p1 and self.nm == p2:
+            so = Point(p2.x - 2*sin(angle)*longueur, p2.y, 0)  
+            voisin = Papillon(so, longueur, longueur, angle)
+            voisin.se.libre = False
+            voisin.sm.libre = False
+            
+        else:
+            print("ERREUR !, tentative de créer un papillon à partir de points incohérents ")
+            return 
+        return voisin
+            
+            
+    def ajouterPapillonHrzt(self, p3):
+        """Ajoute un papillon vertical à partir d'un voisin
+            on vérifie que le papillon aura un géométrie 'normale' """        
+        
+        lvert = self.ne.distance(self.se)
+        lhrzt = self.ne.distance(p3)
+        angle = acos((self.se.distance(p3)**2 - lhrzt**2 - lvert**2) / (-2*lhrzt*lvert))     
+        
+        #assert(lhrzt <= lvert/2 /cos(angle))
+        
+        voisin = Papillon(self.se, lhrzt, lvert, angle)
+        voisin.no.libre = False
+        voisin.so.libre = False
+        voisin.nm.libre = False
+        return voisin
+        
+        
+    def ajouterPapillonVert(self):
+        #Ajout d'un papillon vertical :
+        lvert = self.nm.distance(self.no)
+        lhrzt = self.so.distance(self.sm)
+        angle = abs(acos((self.sm.distance(self.no)**2 - lhrzt**2 - lvert**2) / (-2*lhrzt*lvert)))     
+
+        voisin = Papillon(self.nm, lhrzt, lvert, angle)
+        return voisin
+        
+
             
     def tracer(self):
         self.no.afficherPoint()    
@@ -37,3 +101,16 @@ class Papillon:
         self.se.afficherSegment(self.sm)
         self.sm.afficherSegment(self.so)
         
+
+
+
+
+
+
+
+
+
+
+
+
+
